cf264f7039c51af2360802e0c3a692fe377a6b2f
chiamin <chiaminchung@gmail.com>
2025-11-15 19:52:30 +0800

    Icon test with errorBuilder

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "e727ae16e8a8b0e4902e9d6a71370f6c",
+const RESOURCES = {"main.dart.js": "48f79a02410e7c62d8a1dab4fbdeb766",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"assets/fonts/MaterialIcons-Regular.otf": "0db35ae7a415370b89e807027510caf0",
+"assets/fonts/MaterialIcons-Regular.otf": "066eb6051da2641f9dbe35b373400908",
 "assets/assets/sounds/ding.mp3": "43bd4ea9bf33c74e26a39b679be8fa50",
 "assets/assets/icons/eraser_257647.png": "8cbb5af2ddebc328dd55986e2c9e1bac",
 "assets/assets/icons/divide.png": "0d12904f930bf15e288e4ac291f92a02",
@@ -28,7 +28,7 @@ const RESOURCES = {"main.dart.js": "e727ae16e8a8b0e4902e9d6a71370f6c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "00e35cbbbcdc337f7e6b5b3639ae759b",
-"flutter_bootstrap.js": "91a1435b87548b72a8e296bbeca11058",
+"flutter_bootstrap.js": "455add1c19cbd862f775c297bf51faa7",
 "index.html": "74bd5fb1d65c647fcc29740d913ef766",
 "/": "74bd5fb1d65c647fcc29740d913ef766",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,32 @@ class TestIconApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Icon 測試',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Icon 測試')),
+        appBar: AppBar(
+          title: const Text('Icon測試'),
+        ),
         body: Center(
-          child: Image.asset(
-            'assets/icons/add.png',
-            width: 200,
-            height: 200,
+          child: Container(
+            width: 240,
+            height: 240,
+            color: Colors.yellowAccent, // 背景放超明顯
+            alignment: Alignment.center,
+            child: Image.asset(
+              'assets/icons/add.png',
+              width: 200,
+              height: 200,
+              fit: BoxFit.contain,
+              // 如果載圖失敗，會顯示一個紅色大錯誤 Icon
+              errorBuilder: (context, error, stackTrace) {
+                return const Icon(
+                  Icons.error,
+                  size: 200,
+                  color: Colors.red,
+                );
+              },
+            ),
           ),
         ),
       ),
