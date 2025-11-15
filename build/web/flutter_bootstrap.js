7c98a7ed852e55c6f09d2157a6f046e0de8a907e
chiamin <chiaminchung@gmail.com>
2025-11-15 12:44:34 +0800

    Update web build with correct base-href

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4272077192"
+    serviceWorkerVersion: "1368473561"
   }
 });
