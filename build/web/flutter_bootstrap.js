37e30dbd810f7160d1ea8bbf35b9d8bde3851a41
chiamin <chiaminchung@gmail.com>
2025-11-15 19:23:33 +0800

    Rebuild web with correct base href and assets

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3113270815"
+    serviceWorkerVersion: "3924393663"
   }
 });
