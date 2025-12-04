7068fb06d1c817afed3a423bb9b1bf2a2a97ff1d
chiamin <chiaminchung@gmail.com>
2025-12-04 16:56:06 +0800

    Update source code - 2025-12-04 16:56:06

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3577494978"
+    serviceWorkerVersion: "3850490572"
   }
 });
